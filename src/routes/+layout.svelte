84fe4e5164f12f62c9615b364ffcebf1c889c279
Partab Rai <amrani.partab@gmail.com>
2023-01-25 10:56:58 +0500

    additional features

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,9 @@
   import "../app.css";
 	import Navbar from "../lib/components/Navbar.svelte";
   import Footer from "$lib/components/Footer.svelte";
+	import Progressbar from "../lib/components/Progressbar.svelte";
 </script>
-
+<Progressbar/>
 <main>
   <Navbar/>
   <slot></slot>
